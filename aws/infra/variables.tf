c1852a0d9ff4866fb7ce086389a0da4d6dec2783
janehojy <janehojy@gmail.com>
2023-03-15 10:15:02 +0800

    change vars

--- aws/infra/variables.tf
+++ aws/infra/variables.tf
@@ -12,6 +12,6 @@ variable "tags" {
 
 variable "aws_region" {
   type        = string
-  default     = "us-east-2"
+  default     = "ap-southeast-1"
   description = "AWS region for all resources"
 }
